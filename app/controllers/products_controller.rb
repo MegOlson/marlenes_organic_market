9fd524cbba64e4eb93393e2ef87fb65e0a9c6c5d
mo <meganannetteolson@yahoo.com>
2017-11-17 09:03:09 -0800

    Display homepage and render form to add product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,41 @@
 class ProductsController < ApplicationController
-  
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Your product has been saved!"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :cost, :country)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Number.between(1, 10), country: Faker::HarryPotter.location, created_at: Faker::Date.between_except(1.year.ago, 1.year.from_now, 21.days.ago))
+
+  250.times do |index|
+    reviews = product.reviews.create!(author: Faker::HarryPotter.character, content: Faker::Lorem.paragraph(2), rating: Faker::Number.between(1, 5), created_at: Faker::Date.between_except(1.year.ago, 1.year.from_now, 21.days.ago))
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
