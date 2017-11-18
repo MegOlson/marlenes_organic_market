ccab8175bb13a255329dc5333697efd3a48d3fa2
mo <meganannetteolson@yahoo.com>
2017-11-17 16:13:20 -0800

    Add Capybara specs.

---
+++ spec/features/add_review_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+# describe "the add a review process" do
+#   it "adds a new review" do
+#     visit product_reviews_path
+#     click_link 'Add Review'
+#     fill_in 'Author', :with => 'Megan'
+#     fill_in 'Content', :with => 'lkdflkdf ffdsm ,flkfmd fdsckmfd dskmfd jdmklc fdkmdfc dsckm.'
+#     fill_in 'Rating', :with => '3'
+#     click_on 'Create Review'
+#     expect(page).to have_content 'Reviews:'
+#   end
+#
+#   it "gives error when no author is entered" do
+#     visit new_product_review_path
+#     click_on 'Create Review'
+#     expect(page).to have_content 'errors'
+#   end
+# end
